cc7a9e5e2b68e3d73d97117597570ddcb54c99e1
tbriggs1 <tombriggs93@protonmail.com>
2021-03-08 20:34:07 +0000

    About section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutSection = () => {
+   return(
+       <div>
+           <div className="about-text">
+               <h1>Hello! My Name Is <br><span>Alex Harrison</span></br></h1>
+               <h3>I'm a passoniate photographer and videographer based in <span>West Yorkshire</span>, although adventure takes me everywhere! </h3>
+           </div>
+           <div className="img-text"></div>
+       </div>
+   )
+}
+
+export default AboutSection;

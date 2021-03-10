b8859ff58318212c39a46cb394799192e7728fee
tbriggs1 <tombriggs93@protonmail.com>
2021-03-10 18:25:53 +0000

    about changes

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,8 +3,16 @@ import alex from '../images/Alex.jpg';
 
 const AboutSection = () => {
    return(
-       <div>
-          
+       <div className="mainContent">
+          <div className="about-section">
+              <div className="info">
+                  <h1>Hi my name is <span>Alex Harrsion</span></h1>
+                  <h3>I'm a passionate photographer and videographer based in West Yorkshire, although I like to adventure everywhere</h3>
+              </div>
+              <div className="alexPicture">
+                    <img src={alex} alt="caption of alex" height="650px"/>
+              </div>
+          </div>
        </div>
    )
 }
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutSection from '../components/AboutSection.js';
 import Services from '../components/Services.js';
 import '../styles/pages.scss';
+
 const AboutMe = () => {
     return(
         <div className="page-content">

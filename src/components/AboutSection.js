555d3b2a0915ee5689ca3f17c731872dc6d1e40c
tbriggs1 <tombriggs93@protonmail.com>
2021-03-14 18:39:27 +0000

    parrelex

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 const AboutSection = () => {
    return(
        <div className="main-content">
           <section className="about-section">
               <div className="info">
                   <h2>Hi! I'm <span>Alex Harrsion</span></h2>
-                  <h3>I'm a passionate <span>Photographer</span> and <span>Videographer</span> <br/> based in West Yorkshire, although I like to adventure everywhere</h3>
+                  <h3>I'm a passionate <br/><span>Photographer</span> and <span>Videographer</span> <br/> based in West Yorkshire, although I like to adventure everywhere</h3>
               </div>
           </section>
        </div>

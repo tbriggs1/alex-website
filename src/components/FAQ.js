f751ba6b14bc1c222b101d6b33992ae37a246493
tbriggs1 <tombriggs93@protonmail.com>
2021-03-14 16:02:54 +0000

    aboutsection complete

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -2,42 +2,52 @@ import React from 'react';
 
 const FAQ = () => {
     return(
-        <div>
-            <h1>FAQ</h1>
-            <div title="How Do I start?">
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>
-                            Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
-                        </p>
-                </div>
-                </div>
-                <div title="How Do I start?">
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>
-                            Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
-                        </p>
-                </div>
-                </div>
-                <div title="How Do I start?">
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>
-                            Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
-                        </p>
-                </div>
-                </div>
-                <div title="How Do I start?">
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>
-                            Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
-                        </p>
-                </div>
-                </div>
-        </div>
-        
+        <section className="FAQ-section">
+            <div className="FAQ">
+                <h1>FAQ</h1>
+                <div className="titleline"></div>
+                <div className="FAQ1" title="How Do I start?">
+                        <h3><span>How Do I start?</span></h3>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
+                            </p>
+                        </div>
+                        <div className="underline"></div>
+                    </div>
+                    <div class="FAQ2" title="How Do I start?">
+                    <h3><span>How Do I start?</span></h3>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
+                            </p>
+                    </div>
+                    <div className="underline"></div>
+                    </div>
+                    <div class="FAQ3" title="How Do I start?">
+                    <h3><span>How Do I start?</span></h3>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
+                            </p>
+                    </div>
+                    <div className="underline"></div>
+                    </div>
+                    <div class="FAQ4" title="How Do I start?">
+                    <h3><span>How Do I start?</span></h3>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>
+                                Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
+                            </p>
+                    </div>
+                    <div className="underline"></div>
+                    </div>
+            </div>
+        </section>
     )
 };
 

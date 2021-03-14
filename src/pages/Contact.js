5f7a64014e892bf6f8f8508996de04c929f8b4c6
tbriggs1 <tombriggs93@protonmail.com>
2021-03-14 08:23:14 +0000

    Nav bar added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/contact.scss'
+import Nav from '../components/Nav';
 import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
     return(
+        <div>
+        <Nav />
         <ContactForm />
+        </div>
     )
 }
 
